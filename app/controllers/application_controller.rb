32b31d7477f717e433fd6742b94a7dc44f40a9fc
Dalton Cherry <daltoniam@gmail.com>
2013-12-01 14:37:26 -0800

    beginning user authencation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,19 +10,20 @@ class ApplicationController < RocketPants::Base
     @current_ability ||= Ability.new(current_user)
   end
 
-  def current_user(auth_token = nil)
-
-  	@current_user ||= User.find_by(auth_token: cookies[:auth_token]) if cookies[:auth_token]
-  	#unless not @current_user
-	#  	if auth_token
-	#  		@current_user =
-	#  	end
-	#end
-
+  def current_user(auth_token)
+    if !auth_token
+      auth_token = cookies[:auth_token]
+    end
+      @current_user ||= User.find_by(auth_token: auth_token) if auth_token
   end
   helper_method :current_user
 
+  def set_current_user(user)
+    @current_user = user
+  end
+  helper_method :set_current_user
+
   def authenticate_user!
-    error! :unauthenticated unless current_user
+    error! :unauthenticated unless current_user(params[:auth_token])
   end
 end
